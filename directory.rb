ffb39980533b9285aa7b58efbb93afb6f5df6c49
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-04-07 16:12:42 +0100

    Asking for further information about students

--- directory.rb
+++ directory.rb
@@ -1,13 +1,23 @@
 #Let's put all students into an array
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students, aswell as their hobbies, country of origin, and hieght."
   puts "To finish, just hit return twice"
   students = []
+  while true do
+  puts "name?"
   name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
+    if name.empty?
+      break
+    end
+  puts "hobby?"
+  hobby = gets.chomp
+  puts "Country of origin?"
+  country = gets.chomp
+  puts "height?"
+  height = gets.chomp
+  students << {name: name, cohort: :november, hobby: hobby, country: country,
+  height: height}
+  puts "Now we have #{students.count} students"
   end
   students
 end
@@ -21,7 +31,9 @@ def print(students)
   count = 0
   while count < students.length do
     if students[count][:name].chr.upcase == 'S' && students[count][:name].length < 12
-      puts "#{count + 1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)"
+      puts "#{count + 1}. #{students[count][:name]} (#{students[count][:cohort]} cohort),
+      hobbies inlcude #{students[count][:hobby]}, they are from #{students[count][:country]}
+      and are #{students[count][:height]} tall!"
     end
     count += 1
    end
